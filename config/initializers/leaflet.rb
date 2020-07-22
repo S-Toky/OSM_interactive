33f3477b2f32ed656ba83a4c35ed4fd11d8969a1
S-Toky <stokyr29@gmail.com>
2020-07-23 00:27:41 +0300

    add to master branch

---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,4 @@
+# Leaflet.tile_layer = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+# # You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
+# Leaflet.attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+# Leaflet.max_zoom = 18
